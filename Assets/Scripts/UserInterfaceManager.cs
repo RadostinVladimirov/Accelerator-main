1d5e7a96b87ffd3e12746dc9484d959229e5b39c
MihalakiK <MihalakiK@gmail.com>
2022-06-25 20:06:00 +0300

    Implementation of Buttons and Clock System

--- Assets/Scripts/UserInterfaceManager.cs
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -17,6 +17,38 @@ public class UserInterfaceManager : MonoBehaviour
 
     private string currentPlayerName;
 
+    [SerializeField]
+    private Text Clock;
+
+    [System.Serializable]
+    public class ClockInputs
+    {
+        public int Hour_Clock;
+        public int Minute_Clock;
+
+        public ClockInputs()
+        {
+            Hour_Clock = System.DateTime.Now.Hour;
+            Minute_Clock = System.DateTime.Now.Minute;
+        }
+    }
+
+    [System.Serializable]
+    public class PlayerEnterValue
+    {
+        public int EnterValue;
+        public int TimesGameStarted;
+        public Text playerEnterValueText;
+        public void UpdateEnterValue()
+        {
+            TimesGameStarted = PlayerPrefs.GetInt("TimesGameStarted");
+            TimesGameStarted += 1;
+            EnterValue = TimesGameStarted;
+            playerEnterValueText.text = "Game Played" + " " + EnterValue + " " + "Times";
+            PlayerPrefs.SetInt("TimesGameStarted", EnterValue);
+        }
+    }
+
 
     private void Awake()
     {
@@ -27,6 +59,7 @@ public class UserInterfaceManager : MonoBehaviour
     {
         currentPlayerName = GetSavedPlayerName();
         playerNameText.text = currentPlayerName;
+        EnterTimes.UpdateEnterValue();
     }
 
     public void SetPlayerName()
@@ -42,7 +75,33 @@ public class UserInterfaceManager : MonoBehaviour
         PlayerPrefs.SetString("PlayerName", playerNameText.text);
     }
 
+    public void ResetName()
+    {
+        inputFieldText.text = " ";
+        playerNameText.text = " ";
+        PlayerPrefs.SetString("PlayerName", playerNameText.text);
+    }
+
+    public void OpenGoogleClock(string link)
+    {
+        Application.OpenURL(link);
+    }
+
 
     private string GetSavedPlayerName() => PlayerPrefs.GetString("PlayerName");
 
+    public ClockInputs ClockInputsInstance = new ClockInputs();
+    public PlayerEnterValue EnterTimes = new PlayerEnterValue();
+
+    public void FixedUpdate()
+    {
+        ClockInputsInstance.Hour_Clock = System.DateTime.Now.Hour;
+        ClockInputsInstance.Minute_Clock = System.DateTime.Now.Minute;
+        Clock.text = "" + ClockInputsInstance.Hour_Clock + ":" + ClockInputsInstance.Minute_Clock;
+    }
+
+
 }
+
+
+
