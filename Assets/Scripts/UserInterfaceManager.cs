53fc385ff963a498b47c5d88e1934372dc61e679
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-06-30 08:44:12 +0300

    Implement main menu Functionality and Option menu functionality

--- Assets/Scripts/UserInterfaceManager.cs
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -7,9 +7,21 @@ using UnityEngine;
 public class UserInterfaceManager : MonoBehaviour
 {
     public static UserInterfaceManager Instance { get; private set; }
+    private bool isMainMenu;
 
+    private bool isOptionMenuOpen;
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void MainMenu()
+    {
+        isMainMenu = true;
+    }
+
+    public void OptionMenuPopup()
+    {
+        isOptionMenuOpen = true;
     }
 }
