45401d59d00cdf82195d79c3d08a39ee43dedcef
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-07-27 14:47:21 +0300

    Anchor Drop down and add color to the app

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private Dropdown dropDown;
+   
+    void Start()
+    {
+        
+    }
+
+}
